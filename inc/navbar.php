fa531b5ed517869bc4ed5d7b09161fd9b7f10f61
jeanmcode <jeancarlosmoralesmendozaqgmail.com>
2023-07-08 22:48:23 -0600

    modificando navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -20,40 +20,18 @@
       </a>
 
       <a class="navbar-item">
-        Documentation
+        Acerca de
       </a>
 
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
-    </div>
+      <a class="navbar-item">
+        Contacto
+      </a>
 
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
+          <a class="button is-primary is-rounded">
+            <strong>Administrar</strong>
           </a>
         </div>
       </div>
